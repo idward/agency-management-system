3bfdd925f5e3293fcef64edbd67a2d83d1772c19
idwardluo <apple@idwardluo.local>
2017-09-03 23:04:11 +0800

    update agency-management-system

--- src/app/modules/bonus/create-nod-item/promotion/promotion.component.ts
+++ src/app/modules/bonus/create-nod-item/promotion/promotion.component.ts
@@ -20,42 +20,42 @@ import {NodItem} from "../../../../model/nod/nodItem.model";
   styleUrls: ['./promotion.component.scss']
 })
 export class NodPromotionComponent implements OnInit, OnDestroy {
-  nodItemCount: number = 0;
-  nodItemOptions: OptionItem[] = [];
-  selectedNodItem: string;
-  files: Observable<TreeNode[]>;
-  selectedFiles: TreeNode[];
-  tempCarsData: TreeNode[] = [];
-  display: boolean = false;
-  cars: TreeNode[] = [];
-  carTree: Observable<TreeNode[]>;
-  selectedCars: TreeNode[];
-  serviceTypes: OptionItem[];
-  serviceType: string;
-  selectedServiceType: string;
-  nod: Nod;
-  nodItem: Observable<NodItem []>;
-  isShowServiceType: boolean = false;
-  nodItemSubscription: Subscription;
-  carTreeSubscription: Subscription;
-  currentNodItem: NodItem;
-  parsedData: Object = {};
+  nodItemCount:number = 0;
+  nodItemOptions:OptionItem[] = [];
+  selectedNodItem:string;
+  files:Observable<TreeNode[]>;
+  selectedFiles:TreeNode[];
+  display:boolean = false;
+  cars:TreeNode[] = [];
+  carTree:Observable<TreeNode[]>;
+  selectedCars:TreeNode[];
+  serviceTypes:OptionItem[];
+  serviceType:string;
+  selectedServiceType:string;
+  nod:Nod;
+  nodItem:Observable<NodItem []>;
+  isShowServiceType:boolean = false;
+  nodItemSubscription:Subscription;
+  carTreeSubscription:Subscription;
+  filesSubscription:Subscription;
+  currentNodItem:NodItem;
+  parsedData:Object = {};
 
   constructor(@Inject('BonusService') private _bonusService,
               @Inject('CarTreeService') private _carTreeService,
               @Inject(ConfirmationService) private _confirmService,
-              private store$: Store<any>, private _router: Router,
-              private _route: ActivatedRoute) {
+              private store$:Store<any>, private _router:Router,
+              private _route:ActivatedRoute) {
     const carTreeFilter$ = this.store$.select('carTreeFilter');
     const carDatasFilter$ = this.store$.select('carDatasFilter');
     const nodItemData$ = this.store$.select('nodItemDatas');
     const nodItemDataFilter$ = this.store$.select('nodItemDataFilter');
 
     this.nodItem = Observable.combineLatest(nodItemData$, nodItemDataFilter$,
-      (datas: NodItem[], filter: any) => datas.filter(filter));
+      (datas:NodItem[], filter:any) => datas.filter(filter));
 
     this.files = Observable.zip(nodItemData$, carDatasFilter$,
-      (datas: TreeNode[], filter: any) => {
+      (datas:TreeNode[], filter:any) => {
         if (this.currentNodItem.nodItem_type === 'PROMOTIONAL_RATIO') {
           return filter(this.currentNodItem.nodItem_data['promotional_ratio']);
         } else if (this.currentNodItem.nodItem_type === 'PROMOTIONAL_AMOUNT') {
@@ -66,14 +66,14 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
       });
 
     this.carTree = Observable.combineLatest(nodItemData$, carTreeFilter$,
-      (datas: TreeNode[], filter: any) => {
+      (datas:TreeNode[], filter:any) => {
         if (!_.isNil(this.currentNodItem)) {
           return filter(this.currentNodItem.nodItem_data['cartree_model']);
         }
       });
   }
 
-  ngOnInit(): void {
+  ngOnInit():void {
     this._bonusService.getData().subscribe(data => this.parsedData = data);
 
     this.serviceTypes = SERVICETYPES;
@@ -106,15 +106,18 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnDestroy(): void {
-    this.store$.dispatch({type:'EMPTY_ALL_NODITEMS'});
+  ngOnDestroy():void {
+    this.store$.dispatch({type: 'EMPTY_ALL_NODITEMS'});
+    this.nodItemSubscription.unsubscribe();
+    this.filesSubscription.unsubscribe();
+    this.carTreeSubscription.unsubscribe();
   }
 
   chooseServiceType() {
     this.isShowServiceType = true;
   }
 
-  chooseNodItem(data: any) {
+  chooseNodItem(data:any) {
     this.selectedNodItem = data;
     let currentNodItem = this.nod.nodList.filter(data => data.nodItem_id === this.selectedNodItem)[0];
     this.serviceType = currentNodItem.nodItem_type;
@@ -144,7 +147,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     this.selectedCars = undefined;
   }
 
-  getCommonData(data: any) {
+  getCommonData(data:any) {
     console.log('Data', this.currentNodItem);
 
     let oldData = this.currentNodItem.nodItem_data['setting_condition'];
@@ -154,7 +157,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     this.store$.dispatch({type: 'UPDATE_NODEITEM', payload: this.currentNodItem});
   }
 
-  updateTotalAmount(data: any) {
+  updateTotalAmount(data:any) {
     this.store$.dispatch({type: 'UPDATE_NODEITEM', payload: data});
   }
 
@@ -189,7 +192,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
 
   }
 
-  editCarCategory(evt: Event) {
+  editCarCategory(evt:Event) {
     if (evt['screenX'] === 0 && evt['screenY'] === 0) {
       return false;
     }
@@ -207,7 +210,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     }
 
     if (this.cars.length === 0) {
-      this._carTreeService.getFilesystem()
+      this.carTreeSubscription = this._carTreeService.getFilesystem()
         .subscribe(datas => {
           if (this.currentNodItem.nodItem_type === 'PROMOTIONAL_RATIO') {
             this.currentNodItem.nodItem_data['promotional_ratio'] = datas;
@@ -226,7 +229,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
       .subscribe(keyword => this.store$.dispatch({type: 'SEARCH_KEYWORDS', payload: keyword}));
   }
 
-  onHide(data: any) {
+  onHide(data:any) {
     this.display = false;
     this.selectedCars = data;
 
@@ -237,11 +240,10 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     console.log('selectedCars:', this.selectedCars);
     this.store$.dispatch({type: 'CAR_SELECTED', payload: this.selectedCars});
     this.currentNodItem.nodItem_data['saved_cartree_model'] = this.selectedCars;
-    this.files.subscribe(promotionDatas => {
+    this.filesSubscription = this.files.subscribe(promotionDatas => {
       if (this.serviceType === 'PROMOTIONAL_RATIO') {
         this.currentNodItem.nodItem_data['saved_promotional_ratio'] = promotionDatas;
       } else if (this.serviceType === 'PROMOTIONAL_AMOUNT') {
-        // promotionDatas[0]['total_amount_count'] = 0;
         this.currentNodItem.nodItem_data['saved_promotional_amount'] = promotionDatas;
       }
     });
@@ -249,7 +251,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     this.store$.dispatch({type: 'UPDATE_NODEITEM', payload: this.currentNodItem});
   }
 
-  private createCarTree(data: TreeNode[]): TreeNode[] {
+  private createCarTree(data:TreeNode[]):TreeNode[] {
     var tempData = [];
     for (let i = 0; i < data.length; i++) {
       let car = this._createCarTree(data[i]);
@@ -258,7 +260,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     return tempData;
   }
 
-  private _createCarTree(data: TreeNode): TreeNode {
+  private _createCarTree(data:TreeNode):TreeNode {
     let newData = {};
     if (data.children) {
       if (data.children.length > 0) {
@@ -273,7 +275,7 @@ export class NodPromotionComponent implements OnInit, OnDestroy {
     return newData;
   }
 
-  private _errorHandle(err: any): Observable<any> {
+  private _errorHandle(err:any):Observable<any> {
     console.log('An error occured:' + err);
     return Observable.throw(err.message || err);
   }
